8c82fd4da7dce40f420106c2d61837c5fa6266bf
Masaru Takahashi <matakaha@microsoft.com>
2026-01-27 13:49:29 +0900

    Terraform構成ファイルを追加し、Azure Functions用のリソースを定義

---
+++ infra/main.tf
@@ -0,0 +1,71 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_user_assigned_identity" "func" {
+  name                = "id-${var.function_app_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_storage_account" "func" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
+}
+
+resource "azurerm_role_assignment" "func_storage_blob_owner" {
+  scope                = azurerm_storage_account.func.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_user_assigned_identity.func.principal_id
+}
+
+resource "azurerm_service_plan" "func" {
+  name                = "asp-${var.function_app_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  os_type  = "Linux"
+  sku_name = "FC1"
+}
+
+resource "azurerm_linux_function_app" "func" {
+  name                = var.function_app_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  service_plan_id            = azurerm_service_plan.func.id
+  storage_account_name       = azurerm_storage_account.func.name
+  storage_uses_managed_identity = true
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.func.id]
+  }
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME           = "python"
+    FUNCTIONS_EXTENSION_VERSION        = "~4"
+    AzureWebJobsStorage__accountName   = azurerm_storage_account.func.name
+    AzureWebJobsStorage__credential    = "managedidentity"
+    AzureWebJobsStorage__clientId      = azurerm_user_assigned_identity.func.client_id
+    WEBSITE_RUN_FROM_PACKAGE           = "1"
+  }
+
+  depends_on = [
+    azurerm_role_assignment.func_storage_blob_owner
+  ]
+}
---
+++ infra/variables.tf
@@ -0,0 +1,23 @@
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "japaneast"
+}
+
+variable "function_app_name" {
+  type        = string
+  description = "Function App name"
+  default     = "calcapimatakaha"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+  default     = "rg-calcapimatakaha"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage account name for Functions"
+  default     = "stcalcapimatakaha"
+}
